7e9c4231ffa129f3b40afa8534b5049a3364d6b7
AmogusImpostaSus <152995448+AmogusImpostaSus@users.noreply.github.com>
2023-12-07 18:22:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Hello")
-print("sus")
+class Student:
+    count = 0
+
+    def __init__(self, height=160):
+        self.height = height
+        Student.count += 1
+
+
+print()
+student = Student()
+student1 = Student(height=170)
+student2 = Student(height=150)
+print(student.height)
+print(student1.height)
+print(student2.height)
+print(student.count)
